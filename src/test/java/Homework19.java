d7ea09a8d5e375827ed4b672279555e3a9371313
prachitestpro <prachi.gupta01@testpro.io>
2023-05-17 17:33:57 -0500

    committing changes to Homework19 by Prachi.

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -8,41 +8,26 @@ import java.util.Collection;
 public class Homework19 extends BaseTest {
     @Test
     public void deletePlaylist() throws InterruptedException {
-      // String deletePlaylistMsg = "Deleted Playlist";
 
-       // navigateToPage();
         provideEmail("prachi.gupta01@testpro.io");
         providePassword("te$t$tudent1");
         clickSubmit();
-      //  openPlaylist();
-      //  clickDeletePlaylistBtn();
-       // Assert.assertTrue(getDeletedPlaylistMsg().contains(deletePlaylistMsg));
-      //  System.out.println("step1");
 
-      //  public void openPlaylist()
-       // {
             WebElement playlist = driver.findElement(By.cssSelector(".playlist:nth-child(4)"));
             playlist.click();
-        //}
+        Thread.sleep(2000);
 
-        //System.out.println("step2");
-
-       // public void clickDeletePlaylistBtn() throws InterruptedException
-       // {
             WebElement deletePlaylistBtn = driver.findElement(By.cssSelector(".btn-delete-playlist"));
             deletePlaylistBtn.click();
-       // System.out.println("step3");
 
             WebElement okBtn = driver.findElement(By.cssSelector(".ok"));
             okBtn.click();
-       // System.out.println("step4");
-       // }
-        //public String getDeletedPlaylistMsg() {
+        Thread.sleep(2000);
+
             WebElement notifMsg = driver.findElement(By.cssSelector("div.success.show"));
             Assert.assertTrue(notifMsg.isDisplayed());
-       // Assert.assertEquals(notification.getText(), "Deleted playlist");
+        Thread.sleep(2000);
 
-        //System.out.println("step5");
     }
 
 
